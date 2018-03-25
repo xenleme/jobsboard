9035cb524d943a22696606f6d283d81f94ae87e0
xenleme <xenleme@gmail.com>
2018-03-25 20:55:41 +0300

    add jobs CRUD ability

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,50 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
+
   def index
+    @jobs = Job.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(jobs_params)
+
+    if @job.save
+      redirect_to @job
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @job.update(jobs_params)
+      redirect_to @job
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def jobs_params
+    params.require(:job).permit(:title, :description, :company, :url)
+  end
+
+  def set_job
+    @job = Job.find(params[:id])
   end
 end
