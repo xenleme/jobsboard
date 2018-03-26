46015360192139fd97b40f31c0f870edf4ce6be8
xenleme <xenleme@gmail.com>
2018-03-26 13:48:00 +0300

    update index action in jobs_controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,12 @@ class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   def index
-    @jobs = Job.all.order('created_at DESC')
+    if params[:category].blank?
+      @jobs = Job.all.order('created_at DESC')
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @jobs = Job.where(category_id: @category_id).order('created_at DESC')
+    end
   end
 
   def show
